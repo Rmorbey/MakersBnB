d27f98fb7a5f4f3c1fcd7db1e5152641d2e4488c
timbo-graham <tmgraham29@gmail.com>
2022-05-16 17:16:28 +0100

    We're not proud of it, but we did what we could on the UI

--- app.rb
+++ app.rb
@@ -6,12 +6,22 @@ class MakersBnB < Sinatra::Base
     register Sinatra::Reloader
   end
 
+  enable :sessions
+
   get '/' do
+    $house = session[:description]
+    $contact = session[:contact_details]
     erb :index
   end
 
   get '/list_property' do
     erb :list_property
+  end
+
+  post '/list_property/listed' do
+    session[:description] = params['property_description']
+    session[:contact_details] = params['contact_details']
+    redirect '/'
   end
 
   run! if app_file == $0
--- spec/feature/list_property_spec.rb
+++ spec/feature/list_property_spec.rb
@@ -4,4 +4,13 @@ feature "list property" do
     click_button "List your property"
     expect(page.status_code).to be(200)
   end
+
+  scenario "user can list a property and see it displayed on listings" do
+    visit("/")
+    click_button "List your property"
+    fill_in 'property_description', with: 'A great house'
+    fill_in 'contact_details', with: 'A great person'
+    click_button "Submit"
+    expect(page).to have_content "A great person"
+  end
 end
