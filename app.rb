c91a6e7c433f01bde4456c7d874846655775bcb3
Russell <russellmorbey@hotmail.co.uk>
2022-05-17 10:34:02 +0100

    Resolved conflicts on main.

--- app.rb
+++ app.rb
@@ -1,28 +1,29 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
+require_relative './lib/property'
 
 class MakersBnB < Sinatra::Base
+
+  enable :sessions, :method_overide
+
   configure :development do
     register Sinatra::Reloader
   end
 
-  enable :sessions
-
   get '/' do
-    $house = session[:description]
-    $contact = session[:contact_details]
-    erb :index
+    redirect '/properties'
   end
 
-  get '/list_property' do
-    erb :list_property
+  get '/properties' do
+    @properties = Property.view_all
+    erb :'properties/index'
   end
-
-  post '/list_property/listed' do
-    session[:description] = params['property_description']
-    session[:contact_details] = params['contact_details']
-    redirect '/'
+  get '/properties/new' do
+    erb :'properties/new'
   end
-
+  post '/properties' do
+    Property.add(description: params[:description], contact: params[:contact])
+    redirect '/properties'
+  end
   run! if app_file == $0
 end
