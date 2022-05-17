d644d2d70cbd2f354d38df2d56fe01e4769c373d
Anish <anishkakaiya@outlook.com>
2022-05-17 15:11:19 +0100

    alter table for user foreign key

--- db/01_create_property_database.sql
+++ db/01_create_property_database.sql
@@ -1,2 +1,6 @@
 CREATE TABLE properties(id SERIAL PRIMARY KEY, description VARCHAR(200), contact VARCHAR(600), picture_url VARCHAR(200));
 ALTER TABLE properties ADD COLUMN picture_url VARCHAR(200);
+
+-- Altering properties database to have user_id foreign key. (Made test user database in 02).
+
+ALTER TABLE properties ADD COLUMN user_id INTEGER REFERENCES users (id);
