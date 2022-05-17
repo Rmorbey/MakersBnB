4a83be1d54fbf10f0d3b9e1fee5df3630371a578
Anish <anishkakaiya@outlook.com>
2022-05-17 12:22:21 +0100

    update CREATE TABLE line

--- db/01_create_property_database.sql
+++ db/01_create_property_database.sql
@@ -1,2 +1,2 @@
-CREATE TABLE properties(id SERIAL PRIMARY KEY, description VARCHAR(200), contact VARCHAR(600)), picture_url VARCHAR(200);
+CREATE TABLE properties(id SERIAL PRIMARY KEY, description VARCHAR(200), contact VARCHAR(600), picture_url VARCHAR(200));
 ALTER TABLE properties ADD COLUMN picture_url VARCHAR(200);
